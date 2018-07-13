b561b9b819876e36e9e2daa288dcbe63ee5fa463
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-07-13 00:07:07 -0500

    foundation setup for timer, next 2 make a main page with options to select between the two and return to main, one page for timer, one for stopwatch, then implement stopwatch

---
+++ timer/lib/stopwatch.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'timer.dart';
+
+class Stopwatch extends StatefulWidget {
+  @override
+  _StopwatchState createState() => _StopwatchState();
+}
+
+class _StopwatchState extends State<Stopwatch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+//start -> DITTO
+//set -> DITTO
+//set becomes -> set max stopwatch time
+//reset -> DITTO
+//in timer -> read time left ---VS--- in stopwatch -> read time passed
+//getOriginalTime becomes -> get max stopwatch time
+//getTimePassed -> DITTO
+//getTimeLeft -> does not exist
+//isRunning -> DITTO
+//getFormattedDuration -> DITTO
+//getStringFromFormattedDuration -> DITTO
+//getStringFromDuration -> DITTO
